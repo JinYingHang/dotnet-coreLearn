87812bdd955f4c22cd5aebd706dabdade71b0f9d
yhjin <1242615901@qq.com>
2022-07-07 09:57:45 +0800

    111111

---
+++ NetCore31Demo/Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore31Demo.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- StudentManagerment/Startup.cs
+++ StudentManagerment/Startup.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace StudentManagerment
@@ -25,26 +28,40 @@ namespace StudentManagerment
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers().AddJsonOptions(cfg =>
+            {
+                cfg.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions();
+                developerExceptionPageOptions.
                 app.UseDeveloperExceptionPage();
             }
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("test.html");
+
+
+            //app.UseDefaultFiles(defaultFilesOptions);//默认文件中间件 终端中间件
+            //app.UseStaticFiles();//静态文件中间件
+            app.UseFileServer();
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    //var processName=Process.GetCurrentProcess().ProcessName;
-                    var configVal = _configuration["Mykey"];
-                    await context.Response.WriteAsync(configVal);
+                    throw new Exception("aaaa");
+                    await context.Response.WriteAsync("hello World2啊啊啊");
                 });
+             
             });
         }
     }
